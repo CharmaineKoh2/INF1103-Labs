3331a4082628a1c387ede2d5ab675b87d445dec1
charmaine <2604187@sit.singaporetech.edu.sg>
2026-09-04 15:12:52 +0800

    Update Profile

--- hello.py
+++ hello.py
@@ -3,3 +3,10 @@ print("Welcome here")
 print("My first post!")
 print("=======================================")
 
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
